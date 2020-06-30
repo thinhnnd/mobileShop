f74842c3327104ff0b104ede50009e8efe385759
thinhnnd <16521179@gm.uit.edu.vn>
2020-06-30 23:22:35 +0700

    add some products

--- mobileShop/management/views.py
+++ mobileShop/management/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from management.models import MobilePhone, Branch
 
 # Create your views here.
-def index(req):
-    return HttpResponse("Hellow world")
+
+class PhoneListView(generic.ListView):
+    model = MobilePhone 
+
+def index(request):
+    num_phones = MobilePhone.objects.all().count()
+    num_branches = Branch.objects.all().count()
+    top_ten_phones_list = MobilePhone.objects.all().order_by('-updated_at')[:10]
+    context = {
+        'num_phones': num_phones,
+        'num_branches': num_branches,
+        'top_ten_phones_list': top_ten_phones_list
+    }
+    return render(request, 'index.html', context = context)
